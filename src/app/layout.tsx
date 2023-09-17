53ba696814428d6635bab7164b3dc6eec20f754a
ic3 <yipliangzhang@hotmail.com>
2023-09-17 03:43:45 +0200

    page.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Scrollium',
-  description: 'Scrollium: Lottery',
+  title: 'Scrollium: Raffle',
+  description: 'Scrollium: Raffle',
   icons: {
     icon: '/logo.png',
   },
