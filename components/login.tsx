a1d18bf06d98e8c5c7790f7612c880469a5ac21c
Yip Zhang <yipliangzhang@hotmail.com>
2023-09-18 08:42:29 +0200

    Moved Toast Down

--- components/login.tsx
+++ components/login.tsx
@@ -52,11 +52,11 @@ function Login() {
   return (
     
     <div className="relative min-h-screen flex flex-col items-center justify-center text-center">
-      <Toaster position="top-center" />
       <video autoPlay loop muted className="absolute z-0 w-full h-full object-cover">
         <source src="https://video.wixstatic.com/video/11062b_71b832b59b744d86bf0310d0fceb3055/480p/mp4/file.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
+      <Toaster position="top-center" />
       <div className="z-10 relative flex flex-col items-center mb-10">
         <img className="rounded-full h-56 w-56 items-center -mb-10 justify-center" src="./logo.png" alt="" />
         <h1 className="special-section text-5xl text-white font-bold no-select mb-2 items-center justify-center">SCROLLIUM: RAFFLE</h1>
